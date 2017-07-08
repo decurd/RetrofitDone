927dd24c39f728d0920727f14d19321d5f84c6f1
decurd <coffee5158@naver.com>
2017-07-09 00:36:14 +0900

    ..

--- app/src/main/java/com/nadongbin/retrofitexe/data/remote/SOService.java
+++ app/src/main/java/com/nadongbin/retrofitexe/data/remote/SOService.java
@@ -2,7 +2,11 @@ package com.nadongbin.retrofitexe.data.remote;
 
 import com.nadongbin.retrofitexe.data.model.SOAnswersResponse;
 
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -19,5 +23,10 @@ public interface SOService {
     @GET("/answers?order=desc&sort=activity&site=stackoverflow")
     // Call<SOAnswersResponse> getAnswers(@Query("tagged") String tags);
     Observable<SOAnswersResponse> getAnswers(@Query("tagged") String tags); // rxjava 적용
+
+    @Headers("User-Agent: Android")
+    @FormUrlEncoded
+    @POST("/answers")
+    Observable<SOAnswersResponse> getAnswers1(@Field("field1") String field1, @Field("field2") String field2);
 
 }
